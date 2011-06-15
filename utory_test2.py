8abb3c3b43da5d99a2a8ef109b232c1965246478
Gab_km <the25thcromosome@gmail.com>
2011-06-16 01:16:55 +0900

    assertNotEqualもTestSuiteに入れてみた。

--- utory_test2.py
+++ utory_test2.py
@@ -15,6 +15,21 @@ utory.set_test_dynamicly(SameArgumentsTest2,
                          'base_same_arguments_are_given',
                          same_arguments_test)
 
+class DifferentArgumentsTest2(unittest.TestCase):
+    def base_different_arguments_are_given(self, input, expected):
+        self.assertNotEqual(input, expected)
+
+different_arguments_test = [{'input': 10, 'expected': 20},
+                            {'input': 'afro', 'expected': 'bobo'},
+                            {'input': [1, 2, 3], 'expected': [2, 3, 4, 5]}]
+
+utory.set_test_dynamicly(DifferentArgumentsTest2,
+                         'base_different_arguments_are_given',
+                         different_arguments_test)
+
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(SameArgumentsTest2)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    loader = unittest.TestLoader()
+    suite_same = loader.loadTestsFromTestCase(SameArgumentsTest2)
+    suite_diff = loader.loadTestsFromTestCase(DifferentArgumentsTest2)
+    all_suite = unittest.TestSuite([suite_same, suite_diff])
+    unittest.TextTestRunner(verbosity=2).run(all_suite)
